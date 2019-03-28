7fa9e4a08092ac1d0d14fe50e9aec6d4e8d633c4
Chad Sparrow <csparrow@sugoi.com>
2019-03-28 13:41:52 -0700

    added config module

--- server/server.js
+++ server/server.js
@@ -1,5 +1,25 @@
+const config = require('config');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//HELMET
+app.use(helmet());
+console.log('Helmet running...');
+
+//CONFIGURATION
+console.log('Application Name: ' + config.get('name'));
+
+//MORGAN
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log(`Morgan enabled...`);
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World');
