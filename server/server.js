0e728acabd44d8fc934cc77c3cc9478fe2efc563
Chad Sparrow <csparrow@sugoi.com>
2019-03-29 10:05:50 -0700

    setup config and env variable connection

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('app:startup');
 const config = require('config');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -10,19 +11,20 @@ app.use(express.static('public'));
 
 //HELMET
 app.use(helmet());
-console.log('Helmet running...');
+debug('Helmet running...');
 
 //CONFIGURATION
-console.log('Application Name: ' + config.get('name'));
+debug('Application Name: ' + config.get('name'));
 
 //MORGAN
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
-    console.log(`Morgan enabled...`);
+    debug(`Morgan enabled...`);
 }
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(5000, () => console.log(`Listening on port 5000...`));
+const PORT = config.get('PORT');
+app.listen(PORT, () => debug(`Listening on port 5000...`));
