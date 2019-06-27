c7726ae23551aedefa80b5c36fc7b5633efe7fa9
chadsparrow <csparrow@sugoi.com>
2019-06-27 09:07:33 -0700

    modularized config and db settings

--- server/server.js
+++ server/server.js
@@ -4,15 +4,14 @@ Joi.objectId = require('joi-objectid')(Joi);
 const winston = require('winston');
 require('winston-mongodb');
 require('winston-daily-rotate-file');
+const morgan = require('morgan');
 const config = require('config');
 const express = require('express');
 require('express-async-errors');
 const app = express();
 require('./startup/routes')(app);
-
 const DB_HOST = config.get('database.host');
 require('./startup/db')(DB_HOST);
-require('./startup/config')(app);
 
 // setup winston
 winston.add(winston.transports.DailyRotateFile, { filename: './logs/application-%DATE%.log', maxFiles: '14d' });
@@ -28,6 +27,21 @@ process.on('uncaughtException', ex => {
 process.on('unhandledRejection', ex => {
   throw ex;
 });
+
+//Morgan development API call Logging
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  winston.info(`Morgan enabled...`);
+}
+
+// Check if jwtPrivateKey env variable is set
+if (!config.get('jwtPrivateKey')) {
+  winston.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+}
+
+//Displays environment application is in
+winston.info('Application Name: ' + config.get('name'));
 
 // configures server port
 const SERVER_PORT = config.get('server.port') || 5001;
